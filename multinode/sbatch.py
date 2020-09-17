ed011c1c38f989c656ddb8cbfc37793b6172fc9c
dheim <physics@d-heim.de>
2020-09-17 14:23:16 +0900

    moved output files into multinode folders

--- multinode/sbatch.py
+++ multinode/sbatch.py
@@ -1,6 +1,6 @@
 
 
-def get_sbatch(nodes: int):
+def get_sbatch(nodes, job_path, worker_path, cluster_file):
     s = """#!/bin/bash
 
 #SBATCH --account=GT5DSTC  # Account number
@@ -13,29 +13,19 @@ def get_sbatch(nodes: int):
 #SBATCH --time 00:10:00    # hh|mm|ss
 #SBATCH --mem 60GB         # Memory
 #SBATCH -p S-M             # Job class
-#SBATCH -o 0_launcher.out
-#SBATCH -e 0_launcher.err
-
-# Usage
-# sbatch try_dask.cmd
+#SBATCH -o {}/launcher.out
+#SBATCH -e {}/launcher.err
 
 # Number of nodes (cannot define before SBATCH commands)
 NODES={}
 echo $NODES
-
-# Clean worker folder
-WORKDIR="worker"
-echo rm -r ./$WORKDIR*
-rm -r ./$WORKDIR*
-echo mkdir -p ./$WORKDIR
-mkdir -p ./$WORKDIR
 
 # Select scheduler
 SCHEDULER=$(srun hostname | head -1)
 echo $SCHEDULER
 
 # Start scheduler
-SCHEDFILE="cluster.json"
+SCHEDFILE="{}"
 echo srun -N 1 -n 1 --nodelist=$SCHEDULER dask-scheduler --interface ipogif0 --scheduler-file $SCHEDFILE &
 srun -N 1 -n 1 --nodelist=$SCHEDULER dask-scheduler --interface ipogif0 --scheduler-file $SCHEDFILE &
 
@@ -44,15 +34,8 @@ sleep 20
 
 # Start worker
 WORKERS=$((NODES - 1))
-echo srun -N $WORKERS -n $WORKERS --exclude=$SCHEDULER dask-worker --scheduler-file $SCHEDFILE --local-directory $WORKDIR
-srun -N $WORKERS -n $WORKERS --exclude=$SCHEDULER dask-worker --scheduler-file $SCHEDFILE --local-directory $WORKDIR
+echo srun -N $WORKERS -n $WORKERS --exclude=$SCHEDULER dask-worker --scheduler-file $SCHEDFILE --local-directory {}
+srun -N $WORKERS -n $WORKERS --exclude=$SCHEDULER dask-worker --scheduler-file $SCHEDFILE --local-directory {}
 
-echo HEUREKA
-
-# If cluster is ready, you can use Client(scheduler_file='$SCHEDFILE')
-# - In Jupyter Notebook on login node
-# - In Python from login node
-# - Or directly here:
-# /home/dheim/miniconda3/bin/python try_dask.py
-""".format(nodes, nodes, nodes)
+""".format(nodes, nodes, job_path, job_path, nodes, cluster_file, worker_path, worker_path)
     return s
--- multinode/starter.py
+++ multinode/starter.py
@@ -7,15 +7,25 @@ import multinode.sbatch as sbatch
 
 def start_cluster(nodes: int):
     pid = os.getpid()
-    s = sbatch.get_sbatch(nodes)
-    with open('multinode/try_dask_{}.cmd'.format(pid), 'w') as file:
+
+    job_path = "multinode-{}".format(pid)
+    if not os.path.exists(job_path):
+        os.makedirs(job_path)
+    worker_path = os.path.join(job_path, "worker")
+    if not os.path.exists(worker_path):
+        os.makedirs(worker_path)
+
+    cluster_file = os.path.join(job_path, "cluster.json")
+    s = sbatch.get_sbatch(nodes, job_path, worker_path, cluster_file)
+    starter_script = '{}/starter-script-{}.cmd'.format(job_path, pid)
+    with open(starter_script, 'w') as file:
         file.write(s)
 
     # Popen does not wait for process end
-    subprocess.Popen(['sbatch', 'multinode/try_dask_{}.cmd'.format(pid)],
+    subprocess.Popen(['sbatch', starter_script],
                      stdout=subprocess.PIPE, universal_newlines=True)
 
-    client = Client(scheduler_file='cluster.json')
+    client = Client(scheduler_file=cluster_file)
     client.wait_for_workers(nodes-2)
     time.sleep(5)
 
